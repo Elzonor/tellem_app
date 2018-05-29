56186523fdcc1c031cb316cdabd760698811ad48
Elzonor <lorenzo.ciglioni@gmail.com>
2018-05-29 08:33:34 +0200

    Adds custom page title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
 	
 	protect_from_forgery with: :exception
 	
-	GENERIC_TITLE = " — Anna Vairo Coach"
+	$generic_title = " — Anna Vairo Coach"
 	
 end
