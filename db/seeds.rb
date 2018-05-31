ee929728d88795c0f728832d0b787d96de4df576
Elzonor <lorenzo.ciglioni@gmail.com>
2018-05-31 18:50:07 +0200

    Refines layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-p1 = Post.create(post_title: "Titolo 1", post_abstract: "Abstract 1", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 1")
-p2 = Post.create(post_title: "Titolo 2", post_abstract: "Abstract 2", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 2")
-p3 = Post.create(post_title: "Titolo 3", post_abstract: "Abstract 3", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 3")
+p1 = Post.create(post_title: "Titolo 1", post_abstract: "Abstract 1", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 1", post_info: "Info 1", post_test: "Test 1")
+p2 = Post.create(post_title: "Titolo 2", post_abstract: "Abstract 2", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 2", post_info: "Info 2", post_test: "Test 2")
+p3 = Post.create(post_title: "Titolo 3", post_abstract: "Abstract 3", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 3", post_info: "Info 3", post_test: "Test 3")
