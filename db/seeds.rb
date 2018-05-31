17813e22c34744a3ffce468922ff9db341a12143
Elzonor <lorenzo.ciglioni@gmail.com>
2018-05-31 09:01:29 +0200

    Adds abstract to seeds.rb and a third fake post

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-p1 = Post.create(post_title: "Titolo 1", post_quote: "Citazione 1", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 1")
-p2 = Post.create(post_title: "Titolo 2", post_quote: "Citazione 2", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 2")
+p1 = Post.create(post_title: "Titolo 1", post_abstract: "Abstract 1", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 1")
+p2 = Post.create(post_title: "Titolo 2", post_abstract: "Abstract 2", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 2")
+p3 = Post.create(post_title: "Titolo 3", post_abstract: "Abstract 3", post_body: "Lorem ipsum dolor sit amet consectuetur adiscipit 3")
