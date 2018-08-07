ca88ca8019cb34f21c9f5c4bbcd4a6b9cfe56ba2
Elzonor <lorenzo.ciglioni@gmail.com>
2018-08-07 08:52:30 +0200

    Display each post history as desc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+		@post_history = @post.history_events.all.order("created_at desc")
 	end
 
 	def new
@@ -14,8 +15,8 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.new(post_params)
-		@event_create = @post.history_events.new(post_id: @post.id, event_type_id: 1)
- 		if @post.save && @event_create
+		@event_new = @post.history_events.new(post_id: @post.id, event_type_id: 1)
+ 		if @post.save && @event_new
  			redirect_to(@post)
  		else
  			render :new
@@ -28,8 +29,8 @@ class PostsController < ApplicationController
 
 	def update
 		@post = Post.find(params[:id])
-		@event_update = @post.history_events.new(post_id: @post.id, event_type_id: 2)
-		if @post.update(post_params) && @event_update
+		@event_edit = @post.history_events.new(post_id: @post.id, event_type_id: 2)
+		if @post.update(post_params) && @event_edit
 			redirect_to(@post)
 		else
 			render :edit
@@ -39,6 +40,7 @@ class PostsController < ApplicationController
 	def destroy
 		@post = Post.find(params[:id])
 		@post.destroy
+		@event_delete = @post.history_events.new(post_id: @post.id, event_type_id: 3)
 		redirect_to posts_path
 	end
 
